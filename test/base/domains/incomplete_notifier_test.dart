85e521d84ae23b6c41cbaf1785101a8a6c29e7c9
Werner Fleischer <70745309+wfleischer@users.noreply.github.com>
2023-01-18 23:32:42 +0100

    base: add incomplete_notifier_test

---
+++ test/base/domains/incomplete_notifier_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nwt_reading/src/base/domains/incomplete_notifier.dart';
+
+void main() {
+  test('Build stays on isLoading', () async {
+    final incompleteNotifierProvider =
+        AsyncNotifierProvider<IncompleteNotifier<int>, int>(
+            IncompleteNotifier.new);
+    final container = ProviderContainer();
+    final result = await Future.any([
+      Future.value(container.read(incompleteNotifierProvider.future)),
+      Future.delayed(const Duration(seconds: 1))
+    ]);
+
+    expect(result, null);
+
+    final isLoading = container.read(incompleteNotifierProvider).isLoading;
+
+    expect(isLoading, true);
+  });
+
+  test('Build resolves to init value', () async {
+    const testValue = 'test';
+    final incompleteNotifierProvider =
+        AsyncNotifierProvider<IncompleteNotifier<String>, String>(
+            IncompleteNotifier.new);
+    final container = ProviderContainer();
+    container.read(incompleteNotifierProvider.notifier).init(testValue);
+    final state = await container.read(incompleteNotifierProvider.future);
+
+    expect(state, testValue);
+  });
+}
