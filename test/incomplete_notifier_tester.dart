725c4ad83702331a4a84aa40894ec38cff1bf465
Werner Fleischer <70745309+wfleischer@users.noreply.github.com>
2023-01-27 20:51:43 +0100

    test: add IncompleteNotifierTester

---
+++ test/incomplete_notifier_tester.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mocktail/mocktail.dart' as mocktail;
+import 'package:nwt_reading/src/base/entities/incomplete_notifier.dart';
+
+class Listener<T> extends mocktail.Mock {
+  void call(T? previous, T next);
+}
+
+class IncompleteNotifierTester<T> {
+  IncompleteNotifierTester(this.provider);
+
+  final AsyncNotifierProvider<IncompleteNotifier, T> provider;
+  final listener = Listener<AsyncValue<T>>();
+  ProviderContainer container = ProviderContainer();
+
+  void reset() {
+    mocktail.reset(listener);
+    container = ProviderContainer();
+    container.listen(
+      provider,
+      listener,
+      fireImmediately: true,
+    );
+  }
+}
