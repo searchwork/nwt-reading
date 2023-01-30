69f684b3b5e4dcc6bfb0a873399b42f5cbde6f73
Werner Fleischer <70745309+wfleischer@users.noreply.github.com>
2023-01-30 23:04:04 +0100

    integration_test: add plans and schedule tests (#4)

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -8,6 +8,7 @@ import 'package:nwt_reading/main.dart' as app;
 import 'package:nwt_reading/src/bible_languages/entities/bible_languages.dart';
 import 'package:nwt_reading/src/plans/entities/plan.dart';
 import 'package:nwt_reading/src/plans/entities/plans.dart';
+import 'package:nwt_reading/src/plans/presentations/plan_card.dart';
 import 'package:nwt_reading/src/schedule/entities/events.dart';
 import 'package:nwt_reading/src/schedule/entities/locations.dart';
 import 'package:nwt_reading/src/schedule/entities/schedules.dart';
@@ -18,16 +19,12 @@ void main() async {
   final deepCollectionEquals = const DeepCollectionEquality().equals;
   SharedPreferences.setMockInitialValues({});
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  final ProviderContainer container = await app.main();
-
-  testWidgets('Initialized to system theme', (tester) async {
-    await tester.pumpAndSettle();
-    final actualTheme = await container.read(themeModeProvider.future);
-
-    expect(actualTheme, ThemeMode.system);
-  });
+  final uncontrolledProviderScope = await app.main();
+  final container = uncontrolledProviderScope.container;
+  WidgetController.hitTestWarningShouldBeFatal = true;
 
   testWidgets('Entities are initialized', (tester) async {
+    await tester.pumpWidget(uncontrolledProviderScope);
     await tester.pumpAndSettle();
 
     expect(
@@ -39,6 +36,120 @@ void main() async {
     expect(container.read(schedulesProvider).valueOrNull, isA<Schedules>());
     expect(container.read(bibleLanguagesProvider).valueOrNull,
         isA<BibleLanguages>());
-    expect(container.read(themeModeProvider).valueOrNull, isA<ThemeMode>());
+    expect(container.read(themeModeProvider).valueOrNull, ThemeMode.system);
+  });
+
+  testWidgets('No plans', (tester) async {
+    await tester.pumpWidget(uncontrolledProviderScope);
+
+    expect(find.byKey(const Key('no-plan-yet')), findsOneWidget);
+    expect(find.byType(PlanCard), findsNothing);
+  });
+
+  testWidgets('Add plans', (tester) async {
+    await tester.pumpWidget(uncontrolledProviderScope);
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
+
+    expect(container.read(plansProvider).valueOrNull?.plans.length, 1);
+    expect(find.byKey(const Key('no-plan-yet')), findsNothing);
+    expect(find.byType(PlanCard), findsOneWidget);
+
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
+
+    expect(container.read(plansProvider).valueOrNull?.plans.length, 2);
+    expect(find.byKey(const Key('no-plan-yet')), findsNothing);
+    expect(find.byType(PlanCard), findsNWidgets(2));
+  });
+
+  testWidgets('Check day 34', (tester) async {
+    await tester.pumpWidget(uncontrolledProviderScope);
+    await tester.tap(find.byType(PlanCard).first);
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('day-0')), findsOneWidget);
+    expect(find.byIcon(Icons.check_circle_outline), findsWidgets);
+    expect(find.byIcon(Icons.check_circle), findsNothing);
+    expect(find.byType(Scrollable), findsOneWidget);
+
+    await tester.scrollUntilVisible(
+      find.byKey(const Key('day-33')),
+      500.0,
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.byIcon(Icons.check_circle_outline), findsWidgets);
+    expect(find.byIcon(Icons.check_circle), findsNothing);
+
+    await tester.tap(find
+        .descendant(
+            of: find.byKey(const Key('day-33')),
+            matching: find.byType(IconButton))
+        .first);
+    await tester.pumpAndSettle();
+
+    expect(
+        container
+            .read(plansProvider)
+            .valueOrNull
+            ?.plans
+            .first
+            .bookmark
+            .dayIndex,
+        33);
+    expect(find.byIcon(Icons.check_circle_outline), findsWidgets);
+    expect(find.byIcon(Icons.check_circle), findsWidgets);
+  });
+
+  testWidgets('Uncheck day 10', (tester) async {
+    await tester.pumpWidget(uncontrolledProviderScope);
+    await tester.tap(find.byType(PlanCard).first);
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('day-0')), findsOneWidget);
+    expect(find.byIcon(Icons.check_circle_outline), findsNothing);
+    expect(find.byIcon(Icons.check_circle), findsWidgets);
+    expect(find.byType(Scrollable), findsOneWidget);
+
+    await tester.scrollUntilVisible(
+      find.byKey(const Key('day-10')),
+      500.0,
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.byIcon(Icons.check_circle_outline), findsNothing);
+    expect(find.byIcon(Icons.check_circle), findsWidgets);
+
+    await tester.tap(find
+        .descendant(
+            of: find.byKey(const Key('day-10')),
+            matching: find.byType(IconButton))
+        .first);
+    await tester.pumpAndSettle();
+
+    expect(
+        container
+            .read(plansProvider)
+            .valueOrNull
+            ?.plans
+            .first
+            .bookmark
+            .dayIndex,
+        10);
+    expect(find.byIcon(Icons.check_circle_outline), findsWidgets);
+  });
+
+  testWidgets('Delete plan', (tester) async {
+    await tester.pumpWidget(uncontrolledProviderScope);
+    await tester.pumpAndSettle();
+    await tester.tap(find.byType(PlanCard).last);
+    await tester.pumpAndSettle();
+    await tester.tap(find.byIcon(Icons.delete));
+    await tester.pumpAndSettle();
+
+    expect(container.read(plansProvider).valueOrNull?.plans.length, 1);
+    expect(find.byKey(const Key('no-plan-yet')), findsNothing);
+    expect(find.byType(PlanCard), findsOneWidget);
   });
 }
