eeb40cbf0718b76836f8291040371e84cdd12e40
Werner Fleischer <70745309+wfleischer@users.noreply.github.com>
2023-02-01 13:27:38 +0100

    integration_test: add settled_tester (#29)

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:nwt_reading/main.dart' as app;
 import 'package:nwt_reading/src/bible_languages/entities/bible_languages.dart';
 import 'package:nwt_reading/src/plans/entities/plan.dart';
 import 'package:nwt_reading/src/plans/entities/plans.dart';
@@ -13,19 +12,19 @@ import 'package:nwt_reading/src/schedule/entities/events.dart';
 import 'package:nwt_reading/src/schedule/entities/locations.dart';
 import 'package:nwt_reading/src/schedule/entities/schedules.dart';
 import 'package:nwt_reading/src/settings/stories/theme_mode_story.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
+import '../test/test_plans.dart';
+import 'settled_tester.dart';
 
 void main() async {
   final deepCollectionEquals = const DeepCollectionEquality().equals;
-  SharedPreferences.setMockInitialValues({});
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  final uncontrolledProviderScope = await app.main();
-  final container = uncontrolledProviderScope.container;
   WidgetController.hitTestWarningShouldBeFatal = true;
 
   testWidgets('Entities are initialized', (tester) async {
-    await tester.pumpWidget(uncontrolledProviderScope);
-    await tester.pumpAndSettle();
+    final uncontrolledProviderScope =
+        await SettledTester(tester).uncontrolledProviderScope;
+    final container = uncontrolledProviderScope.container;
 
     expect(
         deepCollectionEquals(
@@ -40,14 +39,16 @@ void main() async {
   });
 
   testWidgets('No plans', (tester) async {
-    await tester.pumpWidget(uncontrolledProviderScope);
+    await SettledTester(tester).uncontrolledProviderScope;
 
     expect(find.byKey(const Key('no-plan-yet')), findsOneWidget);
     expect(find.byType(PlanCard), findsNothing);
   });
 
   testWidgets('Add plans', (tester) async {
-    await tester.pumpWidget(uncontrolledProviderScope);
+    final uncontrolledProviderScope =
+        await SettledTester(tester).uncontrolledProviderScope;
+    final container = uncontrolledProviderScope.container;
     await tester.tap(find.byType(FloatingActionButton));
     await tester.pumpAndSettle();
 
@@ -64,7 +65,10 @@ void main() async {
   });
 
   testWidgets('Check day 34', (tester) async {
-    await tester.pumpWidget(uncontrolledProviderScope);
+    final uncontrolledProviderScope =
+        await SettledTester(tester, sharedPreferences: testPlansPreferences)
+            .uncontrolledProviderScope;
+    final container = uncontrolledProviderScope.container;
     await tester.tap(find.byType(PlanCard).first);
     await tester.pumpAndSettle();
 
@@ -103,8 +107,11 @@ void main() async {
   });
 
   testWidgets('Uncheck day 10', (tester) async {
-    await tester.pumpWidget(uncontrolledProviderScope);
-    await tester.tap(find.byType(PlanCard).first);
+    final uncontrolledProviderScope =
+        await SettledTester(tester, sharedPreferences: testPlansPreferences)
+            .uncontrolledProviderScope;
+    final container = uncontrolledProviderScope.container;
+    await tester.tap(find.byType(PlanCard).last);
     await tester.pumpAndSettle();
 
     expect(find.byKey(const Key('day-0')), findsOneWidget);
@@ -129,27 +136,23 @@ void main() async {
     await tester.pumpAndSettle();
 
     expect(
-        container
-            .read(plansProvider)
-            .valueOrNull
-            ?.plans
-            .first
-            .bookmark
-            .dayIndex,
+        container.read(plansProvider).valueOrNull?.plans.last.bookmark.dayIndex,
         10);
     expect(find.byIcon(Icons.check_circle_outline), findsWidgets);
   });
 
   testWidgets('Delete plan', (tester) async {
-    await tester.pumpWidget(uncontrolledProviderScope);
-    await tester.pumpAndSettle();
+    final uncontrolledProviderScope =
+        await SettledTester(tester, sharedPreferences: testPlansPreferences)
+            .uncontrolledProviderScope;
+    final container = uncontrolledProviderScope.container;
     await tester.tap(find.byType(PlanCard).last);
     await tester.pumpAndSettle();
     await tester.tap(find.byIcon(Icons.delete));
     await tester.pumpAndSettle();
 
-    expect(container.read(plansProvider).valueOrNull?.plans.length, 1);
+    expect(container.read(plansProvider).valueOrNull?.plans.length, 2);
     expect(find.byKey(const Key('no-plan-yet')), findsNothing);
-    expect(find.byType(PlanCard), findsOneWidget);
+    expect(find.byType(PlanCard), findsNWidgets(2));
   });
 }
---
+++ integration_test/settled_tester.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nwt_reading/main.dart' as app;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettledTester {
+  SettledTester(this.tester, {this.sharedPreferences = const {}}) {
+    SharedPreferences.setMockInitialValues(sharedPreferences);
+  }
+
+  final WidgetTester tester;
+  final Map<String, Object> sharedPreferences;
+
+  Future<UncontrolledProviderScope> get uncontrolledProviderScope async {
+    UncontrolledProviderScope uncontrolledProviderScope = await app.main();
+    await tester.pumpWidget(uncontrolledProviderScope);
+    await tester.pumpAndSettle();
+
+    return uncontrolledProviderScope;
+  }
+}
