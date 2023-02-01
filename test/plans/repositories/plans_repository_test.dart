dfeb8a3dd1ddd8d6d2731865c4b15800414f24ef
Werner Fleischer <70745309+wfleischer@users.noreply.github.com>
2023-02-01 11:24:11 +0100

    test: add test_plans

--- test/plans/repositories/plans_repository_test.dart
+++ test/plans/repositories/plans_repository_test.dart
@@ -2,59 +2,14 @@ import 'package:collection/collection.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:nwt_reading/src/plans/entities/plan.dart';
 import 'package:nwt_reading/src/plans/entities/plans.dart';
 import 'package:nwt_reading/src/plans/repositories/plans_repository.dart';
-import 'package:nwt_reading/src/schedule/entities/schedules.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../incomplete_notifier_tester.dart';
+import '../../test_plans.dart';
 
 void main() async {
-  const preferenceKey = 'plans';
-  final Plans testPlans = Plans(const [
-    Plan(
-        id: "5aa4de9e-036b-42cd-8bcb-a92cae46db27",
-        name: "Chronological",
-        scheduleKey: ScheduleKey(
-            type: ScheduleType.chronological,
-            duration: ScheduleDuration.y1,
-            version: "1.0"),
-        language: "en",
-        bookmark: Bookmark(dayIndex: 16, sectionIndex: 0),
-        withEndDate: true,
-        showEvents: true,
-        showLocations: true),
-    Plan(
-        id: "0da6b8a7-ccd4-4270-8058-9e30a3f55ceb",
-        name: "Sequential",
-        scheduleKey: ScheduleKey(
-            type: ScheduleType.sequential,
-            duration: ScheduleDuration.y1,
-            version: "1.0"),
-        language: "en",
-        bookmark: Bookmark(dayIndex: 115, sectionIndex: 0),
-        withEndDate: true,
-        showEvents: true,
-        showLocations: true),
-    Plan(
-        id: "2dab49f3-aecf-4aba-9e91-d75c297d4b7e",
-        name: "Written",
-        scheduleKey: ScheduleKey(
-            type: ScheduleType.written,
-            duration: ScheduleDuration.y1,
-            version: "1.0"),
-        language: "en",
-        bookmark: Bookmark(dayIndex: 364, sectionIndex: 1),
-        withEndDate: true,
-        showEvents: true,
-        showLocations: true)
-  ]);
-  const List<String> testPlansSerialized = [
-    '{"id":"5aa4de9e-036b-42cd-8bcb-a92cae46db27","name":"Chronological","scheduleKey":{"type":0,"duration":2,"version":"1.0"},"language":"en","bookmark":{"dayIndex":16,"sectionIndex":0},"withEndDate":true,"showEvents":true,"showLocations":true}',
-    '{"id":"0da6b8a7-ccd4-4270-8058-9e30a3f55ceb","name":"Sequential","scheduleKey":{"type":1,"duration":2,"version":"1.0"},"language":"en","bookmark":{"dayIndex":115,"sectionIndex":0},"withEndDate":true,"showEvents":true,"showLocations":true}',
-    '{"id":"2dab49f3-aecf-4aba-9e91-d75c297d4b7e","name":"Written","scheduleKey":{"type":2,"duration":2,"version":"1.0"},"language":"en","bookmark":{"dayIndex":364,"sectionIndex":1},"withEndDate":true,"showEvents":true,"showLocations":true}'
-  ];
   const asyncLoadingValue = AsyncLoading<Plans>();
   final emptyPlans = Plans(const []);
   final tester = IncompleteNotifierTester<Plans>(plansProvider);
@@ -88,8 +43,7 @@ void main() async {
   test('Resolves to Shared Preferences', () async {
     tester.reset();
     addTearDown(tester.container.dispose);
-    SharedPreferences.setMockInitialValues(
-        {preferenceKey: testPlansSerialized});
+    SharedPreferences.setMockInitialValues(testPlansPreferences);
     tester.container.read(plansRepositoryProvider);
     final result = await tester.container.read(plansProvider.future);
 
@@ -135,7 +89,7 @@ void main() async {
       await container.read(plansProvider.notifier).addPlan(plan);
     }
     final actualPlansSerialized =
-        sharedPreferences.getStringList(preferenceKey);
+        sharedPreferences.getStringList(plansPreferenceKey);
 
     expect(actualPlansSerialized, testPlansSerialized);
   });
