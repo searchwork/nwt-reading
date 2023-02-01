483e79f524f5e0f668d363432755466c91d044a4
Werner Fleischer <70745309+wfleischer@users.noreply.github.com>
2023-02-01 14:30:47 +0100

    local_build: add running integration test

--- local_build.sh
+++ local_build.sh
@@ -4,6 +4,10 @@ dart format --output=none --set-exit-if-changed .
 dart analyze --fatal-infos
 flutter test
 
+if [[ $(uname) == 'Darwin' ]]; then
+    flutter test integration_test -d macos
+fi
+
 flutter build apk
 flutter build appbundle
 
