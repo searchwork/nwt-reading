1f6d1b0d0096b414d2329b1a6f22705db46e6521
Werner Fleischer <70745309+wfleischer@users.noreply.github.com>
2023-02-09 19:36:28 +0100

    logs: change ProviderLogger to separate file (#37)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nwt_reading/src/bible_languages/repositories/bible_languages_repository.dart';
+import 'package:nwt_reading/src/logs/repositories/provider_logger.dart';
 import 'package:nwt_reading/src/plans/repositories/plans_repository.dart';
 import 'package:nwt_reading/src/schedule/repositories/events_repository.dart';
 import 'package:nwt_reading/src/schedule/repositories/locations_repository.dart';
@@ -12,7 +13,7 @@ import 'src/app.dart';
 
 Future<UncontrolledProviderScope> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final container = ProviderContainer(observers: [Logger()]);
+  final container = ProviderContainer(observers: [ProviderLogger()]);
   container.read(plansRepository);
   container.read(locationsRepository);
   container.read(eventsRepository);
@@ -29,42 +30,4 @@ Future<UncontrolledProviderScope> main() async {
   runApp(uncontrolledProviderScope);
 
   return uncontrolledProviderScope;
-}
-
-class Logger extends ProviderObserver {
-  @override
-  void didAddProvider(
-    ProviderBase provider,
-    Object? value,
-    ProviderContainer container,
-  ) {
-    debugPrint(
-        'didAddProvider: ${provider.name ?? provider.runtimeType} -- value: ${value.toString()}');
-  }
-
-  @override
-  void providerDidFail(ProviderBase provider, Object error,
-      StackTrace stackTrace, ProviderContainer container) {
-    debugPrint(
-        'didAddProvider: ${provider.name ?? provider.runtimeType} -- error: ${error.toString()}');
-  }
-
-  @override
-  void didDisposeProvider(
-    ProviderBase provider,
-    ProviderContainer container,
-  ) {
-    debugPrint('didAddProvider: ${provider.name ?? provider.runtimeType}');
-  }
-
-  @override
-  void didUpdateProvider(
-    ProviderBase provider,
-    Object? previousValue,
-    Object? newValue,
-    ProviderContainer container,
-  ) {
-    debugPrint(
-        'didAddProvider: ${provider.name ?? provider.runtimeType} -- newValue: $newValue');
-  }
 }
