e0aab562aae9f40290ec41792b579284d938fbdb
Werner Fleischer <70745309+wfleischer@users.noreply.github.com>
2023-08-06 16:18:32 +0200

    integration_test: fix onScreenshot args

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -4,7 +4,7 @@ import 'package:integration_test/integration_test_driver_extended.dart';
 Future<void> main() async {
   try {
     await integrationDriver(
-      onScreenshot: (String filePath, List<int> bytes) async {
+      onScreenshot: (String filePath, List<int> bytes, [args]) async {
         final File image = await File(filePath).create(recursive: true);
         image.writeAsBytesSync(bytes);
 
