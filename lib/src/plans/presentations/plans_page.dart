54b794848d1d94519d3fd0e1323b02a9cc11eb15
Werner Fleischer <70745309+wfleischer@users.noreply.github.com>
2024-12-06 21:58:13 +0100

    plans: fix repeatedly showing the whats_new dialog

--- lib/src/plans/presentations/plans_page.dart
+++ lib/src/plans/presentations/plans_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nwt_reading/src/localization/app_localizations_getter.dart';
-import 'package:nwt_reading/src/plans/entities/plans.dart';
-import 'package:nwt_reading/src/plans/presentations/plan_card.dart';
 import 'package:nwt_reading/src/plans/presentations/plan_edit_dialog.dart';
 import 'package:nwt_reading/src/plans/presentations/plans_grid.dart';
 import 'package:nwt_reading/src/settings/stories/settings_story.dart';
@@ -10,46 +8,54 @@ import 'package:nwt_reading/src/whats_new/presentations/whats_new_dialog.dart';
 
 import '../../settings/presentations/settings_page.dart';
 
-class PlansPage extends ConsumerWidget {
+class PlansPage extends ConsumerStatefulWidget {
   const PlansPage({super.key});
   static const routeName = '/';
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => FutureBuilder(
-        future: _showWhatsNewDialog(context, ref),
-        builder: (context, snapshot) => Scaffold(
-          appBar: AppBar(
-            title: Text(context.loc.plansPageTitle),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.settings),
-                onPressed: () {
-                  Navigator.restorablePushNamed(
-                      context, SettingsPage.routeName);
-                },
-              ),
-            ],
-          ),
-          body: PlansGrid(),
-          floatingActionButton: FloatingActionButton(
-            tooltip: context.loc.plansPageAddPlanTooltip,
-            onPressed: () => showDialog<String>(
-              context: context,
-              builder: (BuildContext context) => PlanEditDialog(),
-            ),
-            child: const Icon(Icons.add),
-          ),
-        ),
-      );
+  PlansPageState createState() => PlansPageState();
+}
 
-  List<PlanCard> buildPlansGrid(Plans plans) =>
-      plans.plans.map((plan) => PlanCard(plan.id)).toList();
+class PlansPageState extends ConsumerState<PlansPage> {
+  @override
+  void initState() {
+    super.initState();
 
-  Future<void> _showWhatsNewDialog(BuildContext context, WidgetRef ref) async {
-    final seenWhatsNewVersion =
-        (await ref.read(settingsProvider.future)).seenWhatsNewVersion;
     WidgetsBinding.instance.addPostFrameCallback((duration) {
-      showWhatsNewDialog(context, ref, seenWhatsNewVersion);
+      callWhatsNewDialog(context);
     });
   }
+
+  void callWhatsNewDialog(BuildContext context) async {
+    final seenWhatsNewVersion =
+        (await ref.read(settingsProvider.future)).seenWhatsNewVersion;
+
+    if (context.mounted) {
+      showWhatsNewDialog(context, ref, seenWhatsNewVersion);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(context.loc.plansPageTitle),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                Navigator.restorablePushNamed(context, SettingsPage.routeName);
+              },
+            ),
+          ],
+        ),
+        body: PlansGrid(),
+        floatingActionButton: FloatingActionButton(
+          tooltip: context.loc.plansPageAddPlanTooltip,
+          onPressed: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => PlanEditDialog(),
+          ),
+          child: const Icon(Icons.add),
+        ),
+      );
 }
